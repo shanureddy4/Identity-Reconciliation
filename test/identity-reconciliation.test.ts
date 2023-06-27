83b59224d392a43483f86b97772ed816ec2a9021
bogathi reddy <bogathi.reddy@powerschool.com>
2023-06-27 10:07:02 +0530

    initial commit - cdk template

---
+++ test/identity-reconciliation.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as IdentityReconciliation from '../lib/identity-reconciliation-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/identity-reconciliation-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new IdentityReconciliation.IdentityReconciliationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
