3f69d4293d6397fc1c0c8eb8794a18854c551e0c
bogathi reddy <bogathi.reddy@powerschool.com>
2023-07-03 08:07:58 +0530

    handler for lambda execution

--- functions/user/user-handler.ts
+++ functions/user/user-handler.ts
@@ -1,9 +1,43 @@
+import { IUserService } from './interface/iuser-service';
+import { UserDetailsIOC } from './user-loader';
+import { SecretManager } from '../aws/secret-manager';
+
 exports.handler = async function (event: any) {
-    console.log('request:', JSON.stringify(event, undefined, 2));
+  try {
+
+    console.log("event log",event);
+    // parse the event body to JSON format
+    const identityData:IdentityData = JSON.parse(event.body);
+    if(identityData && !identityData.email && !identityData.phoneNumber)
+    {
+      throw new Error("please provide atleast email or phone number")
+    }
+
+    // assign environment variables
+    await SecretManager.setDbConfig()
+
+    // call the getUserContactDetails method on the UserService instance
+    const userService = UserDetailsIOC.getContainer().get<IUserService>(Symbol.for('IUserService'));
+    const contactData = await userService.getUserContactDetails(identityData);
+
+    // return the contactData as the response
     return {
       statusCode: 200,
-      headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
-      body: JSON.stringify({ Message: 'Hello User!!' })
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({ contactData})
     };
-  };
-  
+  } catch (error: any) {
+    console.error(`getUserContactDetailsHandler - Error fetching user contact details: ${error.message}`);
+    return {
+      statusCode: 500,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+};
